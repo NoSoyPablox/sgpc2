7f9dfe830c10554b23a3a93694d7b911a2afd330
NoSoyPablox <zS20015755@estudiantes.uv.mx>
2024-04-01 10:53:46 -0600

    Fix Entity functionality

--- Pages/DatosDeCliente.xaml.cs
+++ Pages/DatosDeCliente.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +12,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -25,6 +29,7 @@ namespace SGSC.Pages
             InitializeComponent();
         }
 
+
         private void btnContinue_Click(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrEmpty(tbCURP.Text) || string.IsNullOrEmpty(tbName.Text) || string.IsNullOrEmpty(tbFirstSurname.Text))
@@ -37,14 +42,56 @@ namespace SGSC.Pages
 
                 if (Validator.ValidateCURP(tbCURP.Text) && Validator.ValidateNames(tbName.Text) && Validator.ValidateNames(tbFirstSurname.Text))
                 {
+                    /*using (ModelContainer db = new ModelContainer())
+                    {
+                        Customer customerToRegister = new Customer();
+                        customerToRegister.Curp = "CUMP01050123";
+                        customerToRegister.Name = "Juan";
+                        customerToRegister.FirstSurname = "Mesa";
+                        customerToRegister.SecondSurname = "Murrieta";
+
+                        db.Customers.Add(customerToRegister);
+
+                        try
+                        {
+                            // Guardamos los cambios en la base de datos
+                            db.SaveChanges();
+                            Console.WriteLine("Cliente registrado exitosamente.");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Ocurrió un error al intentar registrar el cliente: " + ex.Message);
+                        }
+                    }*/
+
+                    using (ModelContainer db = new ModelContainer())
+                    {
+                        WorkCenter workCenter = new WorkCenter();
+                        workCenter.Name = "Centro de trabajo 1";
+                        workCenter.Address = "Calle 1";
+                        workCenter.PhoneNumber = "1234567890";
+
+                        db.WorkCenters.Add(workCenter);
+                        try
+                        {
+                            db.SaveChanges();
+                        }catch (Exception ex)
+                        {
+                            Console.WriteLine("Ocurrió un error al intentar registrar el centro de trabajo: " + ex.Message);
+                        }
+
+                    };
+
                     MessageBox.Show("Datos guardados correctamente", "Operación realizada con exito", MessageBoxButton.OK, MessageBoxImage.Information);
+
                 }
                 else
                 {
                     MessageBox.Show("Los datos introducidos son incorrectos", "Información invalida", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
-        }
+        }   
+
 
         private void tbName_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
