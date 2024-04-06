3185c36ffab091297c2b75786c75d2a817769de0
CardoneZ <cardonezapatayt@gmail.com>
2024-04-06 17:03:24 -0600

    AddressInformation CU working

--- Pages/DatosDeContacto.xaml.cs
+++ Pages/DatosDeContacto.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,79 @@ namespace SGSC.Pages
     /// </summary>
     public partial class DatosDeContacto : Page
     {
+        private int customer;
         public DatosDeContacto()
         {
+            customer = 1;
             InitializeComponent();
+            //UpdateContactInformation();
         }
+
+        private void AddContactInformation(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                if (string.IsNullOrWhiteSpace(Tb_Email.Text) || string.IsNullOrWhiteSpace(Tb_PhoneNumberOne.Text) ||
+                    string.IsNullOrWhiteSpace(Tb_PhoneNumberTwo.Text))
+                {
+                    MessageBox.Show("Por favor, complete todos los campos solicitados.");
+                    return;
+                }
+
+
+                var newCustomerContactInfo = new CustomerContactInfo
+                {
+                  PhoneNumberOne = Tb_PhoneNumberOne.Text,
+                  PhoneNumberTwo = Tb_PhoneNumberTwo.Text,
+                  Email = Tb_Email.Text,
+                };
+
+                if (customer != 0)
+                {
+                    newCustomerContactInfo.CustomerContactInfoId = customer;
+                }
+
+
+                using (sgscEntities context = new sgscEntities())
+                {
+                    context.CustomerContactInfoes.AddOrUpdate();
+                    context.SaveChanges();
+                }
+
+                MessageBox.Show("Los datos de contacto se han guardado correctamente.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al intentar guardar los datos de contacto: " + ex.Message);
+            }
+        }
+
+        /*private void UpdateContactInformation()
+        {
+            try
+            {
+
+                using (var context = new sgscEntities())
+                {
+                    var customerData = context.CustomerContactInfoes
+                        .Where(customerDb => customerDb. == customer)
+                        .FirstOrDefault();
+
+
+                    if (customerData != null)
+                    {
+                        Tb_PhoneNumberOne = customerData.PhoneNumberOne;
+                        Tb_PhoneNumberTwo = customerData.PhoneNumberTwo;    
+                        Tb_Email = customerData.Email;  
+                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al intentar obtener los datos de contacto: " + ex.Message);
+            }
+        }*/
     }
 }
