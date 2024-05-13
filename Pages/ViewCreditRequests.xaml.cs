92b40e2bc83cae4e5722dfcda1c4bca9c4e42ef8
CardoneZ <cardonezapatayt@gmail.com>
2024-05-12 21:21:22 -0600

    ViewCreditRequestNotWorking

---
+++ Pages/ViewCreditRequests.xaml.cs
@@ -0,0 +1,171 @@
+﻿using SGSC.Frames;
+using SGSC.Utils;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SGSC.Pages
+{
+    /// <summary>
+    /// Interaction logic for ViewCreditRequests.xaml
+    /// </summary>
+    public partial class ViewCreditRequests : Page
+    {
+        private int currentPage = 0;
+        private int pageSize = 7;
+        private DispatcherTimer timer;
+
+        public ViewCreditRequests()
+        {
+            InitializeComponent();
+            GetCreditRequests();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop(); 
+            SearchCreditRequests();
+        }
+
+        private void SearchCreditRequests()
+        {
+            string searchText1 = tbRfc.Text.Trim();
+            string searchText2 = tbCreationDate.Text.Trim();
+            string searchText3 = tbStatus.Text.Trim();
+
+            try
+            {
+                using (sgscEntities db = new sgscEntities())
+                {
+                    var creditRequests = (from cr in db.CreditRequests
+                                          join c in db.Customers on cr.CustomerId equals c.CustomerId
+                                          where c.Name.Contains(searchText1) &&
+                                                c.FirstSurname.Contains(searchText2) &&
+                                                c.SecondSurname.Contains(searchText3)
+                                          select new
+                                          {
+                                              cr.CreditRequestId,
+                                              cr.Status,
+                                              cr.CreationDate,
+                                              CustomerName = c.Name + " " + c.FirstSurname + " " + c.SecondSurname,
+                                              c.Rfc,
+                                          }).Skip(currentPage * pageSize).Take(pageSize).ToList();
+
+                    if (creditRequests.Any())
+                    {
+                        creditRequestsDataGrid.ItemsSource = creditRequests;
+                    }
+                    else
+                    {
+                        currentPage--;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al intentar buscar las solicitudes de crédito. Por favor, inténtelo de nuevo más tarde.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            timer.Stop(); 
+            timer.Start();
+        }
+
+        private void GetCreditRequests()
+        {
+            try
+            {
+                using (sgscEntities db = new sgscEntities())
+                {
+                    var creditRequests = (from cr in db.CreditRequests
+                                          join c in db.Customers on cr.CustomerId equals c.CustomerId
+                                          select new
+                                          {
+                                              cr.CreditRequestId,
+                                              cr.Status,
+                                              cr.CreationDate,
+                                              CustomerName = c.Name + " " + c.FirstSurname + " " + c.SecondSurname,
+                                              c.Rfc,
+                                          }).Take(pageSize).ToList();
+
+                    if (creditRequests.Any())
+                    {
+                        creditRequestsDataGrid.ItemsSource = creditRequests;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(" Sex Ocurrió un error al intentar obtener las solicitudes de crédito. Por favor, inténtelo de nuevo más tarde.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            UserSession.LogOut();
+        }
+
+        private void NextPageRequest(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                currentPage++;
+
+                using (sgscEntities db = new sgscEntities())
+                {
+                    var creditRequests = (from cr in db.CreditRequests
+                                          join c in db.Customers on cr.CustomerId equals c.CustomerId
+                                          select new
+                                          {
+                                              cr.CreditRequestId,
+                                              cr.Status,
+                                              cr.CreationDate,
+                                              CustomerName = c.Name + " " + c.FirstSurname + " " + c.SecondSurname,
+                                              c.Rfc,
+                                          }).Skip(currentPage * pageSize).Take(pageSize).ToList();
+
+                    if (creditRequests.Any())
+                    {
+                        creditRequestsDataGrid.ItemsSource = creditRequests;
+                    }
+                    else
+                    {
+                        currentPage--;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al intentar obtener las solicitudes de crédito. Por favor, inténtelo de nuevo más tarde.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
