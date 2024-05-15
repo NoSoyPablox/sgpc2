c813c8d2dd1b607b210ecb9922f10169f6f562f2
CardoneZ <cardonezapatayt@gmail.com>
2024-05-15 14:22:09 -0600

    View Credit Requests Update

---
+++ Model/CreditRequest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGSC
+{
+    public partial class CreditRequest
+    {
+
+
+        public enum RequestStatus
+        {
+            Captured,
+            Pending,
+            Rejected,
+            WaitingForCorrection,
+            Authorized,
+            Paid,
+            
+        }
+
+        public static string RequestStatusToString(RequestStatus requestStatus)
+        {
+            switch (requestStatus)
+            {
+                case RequestStatus.Paid:
+                    return "Pagado";
+                case RequestStatus.Authorized:
+                    return "Aprobada";
+                case RequestStatus.Rejected:
+                    return "Rechazada";
+                case RequestStatus.Captured:
+                    return "Capturada";
+                case RequestStatus.WaitingForCorrection:
+                    return "En espera de corrección";
+                 case RequestStatus.Pending:
+                    return "Pendiente";
+                default:
+                    return "Estado de solicitud desconocido";
+            }
+        }
+    }
+}
