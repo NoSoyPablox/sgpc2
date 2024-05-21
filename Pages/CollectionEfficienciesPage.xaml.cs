3ee1580b5cd21475ef3798bd7532151dde80324f
MangoFizz <ccs81.mr@gmail.com>
2024-05-20 22:17:37 -0600

    Post-merge fixes

--- Pages/CollectionEfficienciesPage.xaml.cs
+++ Pages/CollectionEfficienciesPage.xaml.cs
@@ -56,6 +56,7 @@ namespace SGSC.Pages
             InitializeComponent();
             CreditRequestId = creditRequestId;
             UserSessionFrame.Content = new UserSessionFrame();
+            creditAdvisorSidebar.Content = new CreditAdvisorSidebar("creditRequest");
             this.Loaded += CollectionEfficienciesPage_Loaded;
         }
 
